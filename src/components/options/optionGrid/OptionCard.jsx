d16fc015375d1e90eae64ac90b49cc51d018e79a
sey223 <seydoukane11@gmail.com>
2019-01-26 15:25:34 +0100

    update
    MAJ

--- src/components/options/optionGrid/OptionCard.jsx
+++ src/components/options/optionGrid/OptionCard.jsx
@@ -5,6 +5,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Media } from 'react-md';
+import '~react-md/src/scss/react-md';
 
 const OptionCard = props => {
   return (
