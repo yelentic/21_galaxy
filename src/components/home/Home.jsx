86cb3595441cf55837f0caf0ea038119e987e868
bgs365 <beydisanogo@gmail.com>
2019-01-30 22:19:13 +0100

    [ADD] home without compennent translation

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,9 +5,9 @@ import './Home.css';
 import Footer from '../footer/Footer';
 import OptionsGrid from '../options/optionGrid/OptionGrid.jsx';
 import Typography from '@material-ui/core/Typography';
-
 import Tabs from 'react-tabs-redux/es/components/Tabs';
 import { TabContent, TabLink } from 'react-tabs-redux';
+import { withNamespaces } from 'react-i18next';
 
 function TabContainer(props) {
   return (
@@ -17,7 +17,7 @@ function TabContainer(props) {
   );
 }
 
-export default class Home extends Component {
+class Home extends Component {
   constructor() {
     super();
     this.state = {
@@ -40,6 +40,8 @@ export default class Home extends Component {
       display: this.state.shown ? 'none' : 'block'
     };
 
+    const { t } = this.props;
+
     return (
       <div>
         <div class="overlay image">
@@ -52,27 +54,21 @@ export default class Home extends Component {
 
         <Jumbotron>
           <Image src="assets/21G Lg3.png" circle className="profile-pic" />
-          <p>
-            Réseau d’experts internationaux, ancré en France et implantés sur 3
-            continents dont 27 pays.{' '}
-          </p>
+          <p>{t('devise')} </p>
           <Link to="/">
             <Button bsStyle="primary" onClick={this.toggle.bind(this)}>
-              Découvrir 21 GALAXY
+              {t('header.discover21Galaxy')}
             </Button>
           </Link>
           <p style={hidden} id="about">
-            21 Galaxy vous accompagne sur tout le cycle de vie de votre
-            entreprise, de sa création jusqu’à son évolution. L’objectif est de
-            pérenniser votre activité et d’optimiser votre croissance et vos
-            opportunités d’affaires.
+            {t('about')}
           </p>
         </Jumbotron>
         <div className="overlay separator">
           &nbsp;{' '}
           <h1 style={{ color: 'white', textAlign: 'center' }}>
             {' '}
-            <i>21 Galaxy le partenaire de votre succès</i>
+            <i>{t('about2')}</i>
           </h1>{' '}
         </div>
 
@@ -86,46 +82,31 @@ export default class Home extends Component {
           &nbsp;{' '}
           <h1 style={{ color: 'white', textAlign: 'center' }}>
             {' '}
-            Nous vous accompagnons dans tous vos projets de A à Z{' '}
+            {t('about3')}{' '}
           </h1>{' '}
         </div>
 
         <div className={'nosoffres'}>
           <hr /> <br />
-          <h1> Notre fonctionnement </h1>
+          <h1> {t('ourOffers.title')} </h1>
           <Image src="assets/Process.jpg" circle className="profile-pic" />
           <Tabs>
             <TabLink className="tableau" to="tab1">
-              Prise de contact{' '}
+              {t('ourOffers.contact')}{' '}
             </TabLink>
             <TabLink className="tableau" to="tab2">
               {' '}
-              Proposition d'intervention
+              {t('ourOffers.interventionProposal')}
             </TabLink>
             <TabLink className="tableau" to="tab3">
-              Suivi de la mission{' '}
+              {t('ourOffers.missionTracking')}{' '}
             </TabLink>
 
             <TabContent for="tab1">
               {' '}
               <div className="presentation">
                 {' '}
-                <p className="text">
-                  On sait depuis longtemps que travailler avec du texte lisible
-                  et contenant du sens est source de distractions, et empêche de
-                  se concentrer sur la mise en page elle-même. L'avantage du
-                  Lorem Ipsum sur un texte générique comme 'Du texte. Du texte.
-                  Du texte.' est qu'il possède une distribution de lettres plus
-                  ou moins normale, et en tout cas comparable avec celle du
-                  français standard. De nombreuses suites logicielles de mise en
-                  page ou éditeurs de sites Web ont fait du Lorem Ipsum leur
-                  faux texte par défaut, et une recherche pour 'Lorem Ipsum'
-                  vous conduira vers de nombreux sites qui n'en sont encore qu'à
-                  leur phase de construction. Plusieurs versions sont apparues
-                  avec le temps, parfois par accident, souvent
-                  intentionnellement (histoire d'y rajouter de petits clins
-                  d'oeil, voire des phrases embarassantes).
-                </p>{' '}
+                <p className="text">{t('ourOffers.contactDropdown')}</p>{' '}
               </div>
             </TabContent>
             <TabContent for="tab2">
@@ -133,19 +114,7 @@ export default class Home extends Component {
               <div className="presentation">
                 {' '}
                 <p className="text">
-                  texte lisible et contenant du sens est source de distractions,
-                  et empêche de se concentrer sur la mise en page elle-même.
-                  L'avantage du Lorem Ipsum sur un texte générique comme 'Du
-                  texte. Du texte. Du texte.' est qu'il possède une distribution
-                  de lettres plus ou moins normale, et en tout cas comparable
-                  avec celle du français standard. De nombreuses suites
-                  logicielles de mise en page ou éditeurs de sites Web ont fait
-                  du Lorem Ipsum leur faux texte par défaut, et une recherche
-                  pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui
-                  n'en sont encore qu'à leur phase de construction. Plusieurs
-                  versions sont apparues avec le temps, parfois par accident,
-                  souvent intentionnellement (histoire d'y rajouter de petits
-                  clins d'oeil, voire des phrases embarassantes).
+                  {t('ourOffers.interventionProposalDropdown')}
                 </p>{' '}
               </div>
             </TabContent>
@@ -154,19 +123,7 @@ export default class Home extends Component {
               <div className="presentation">
                 {' '}
                 <p className="text">
-                  texte lisible et contenant du sens est source de distractions,
-                  et empêche de se concentrer sur la mise en page elle-même.
-                  L'avantage du Lorem Ipsum sur un texte générique comme 'Du
-                  texte. Du texte. Du texte.' distribution de lettres plus ou
-                  moins normale, et en tout cas comparable avec celle du
-                  français standard. De nombreuses suites logicielles de mise en
-                  page ou éditeurs de sites Web ont fait du Lorem Ipsum leur
-                  faux texte par défaut, et une recherche pour 'Lorem Ipsum'
-                  vous conduira vers de nombreux sites qui n'en sont encore qu'à
-                  leur phase de construction. Plusieurs versions sont apparues
-                  avec le temps, parfois par accident, souvent
-                  intentionnellement (histoire d'y rajouter de petits clins
-                  d'oeil, voire des phrases embarassantes).
+                  {t('ourOffers.missionTrackingDropdown')}
                 </p>{' '}
               </div>{' '}
             </TabContent>
@@ -177,3 +134,4 @@ export default class Home extends Component {
     );
   }
 }
+export default withNamespaces('translation')(Home);
